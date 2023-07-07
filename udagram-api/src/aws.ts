10e40055f76b42d2333399b2c962df8d9e652627
khoatran <khoatrando516@gmail.com>
2023-07-07 23:11:43 +0700

    update code

--- udagram-api/src/aws.ts
+++ udagram-api/src/aws.ts
@@ -2,10 +2,13 @@ import AWS = require("aws-sdk");
 import { config } from "./config/config";
 
 //Credentials are auto set according to the documentation https://docs.aws.amazon.com/sdk-for-javascript/v2/developer-guide/setting-credentials-node.html and the default profile is "Default anyway"
-
+AWS.config.update({
+  secretAccessKey: config.secretAccessKey,
+  accessKeyId: config.accessKeyId,
+  region: config.aws_region,
+})
 export const s3 = new AWS.S3({
   signatureVersion: "v4",
-  region: config.aws_region,
   params: { Bucket: config.aws_media_bucket },
 });
 
